d2a3dba6b9a7731355ef0974f0295d3e42faf4f0
Mohamed Tharwat <mtua7422@gmail.com>
2026-02-15 18:43:19 +0200

    feat: first GUI window using Dioxus and integrated geometry engine

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
 mod types;
 mod geometry;
-mod object; // الملف الجديد
+mod object;
 
 use object::VPoint;
 
 fn main() {
-    // إنشاء أول نقطة فالنتينا حقيقية
-    let p1 = VPoint::new(1, "A", 100.0, 50.0);
+    // في الإصدارات الحديثة نستخدم LaunchBuilder أو مباشرة Launch
+    launch(App);
+}
 
-    println!("--- Valentina Object Created ---");
-    println!("Name: {}", p1.metadata.name);
-    println!("ID: {}", p1.metadata.id);
-    println!("Coordinates: ({}, {})", p1.x(), p1.y());
+#[component]
+fn App() -> Element {
+    // استخدام Signal لإدارة الحالة (State)
+    let mut point = use_signal(|| VPoint::new(1, "Point A", 150.0, 150.0));
+    let mut angle = use_signal(|| 0.0);
 
-    // تخيل إننا بنلفف النقطة دي
-    let origin = geometry::Point2D::new(0.0, 0.0);
-    let rotated_coords = p1.coords.rotate(&origin, 90.0);
-    
-    println!("After 90 deg rotation: ({:.2}, {:.2})", rotated_coords.x, rotated_coords.y);
+    rsx! {
+        // إذا لم يكن لديك ملف CSS بعد، يمكنك كتابة التنسيق هنا مباشرة لتجنب أخطاء المسارات
+        style { {include_str!("../assets/main.css")} }
+        
+        div { id: "container",
+            div { id: "sidebar",
+                h2 { "Valentina-Oxidized 🦀" }
+                hr {}
+                div { class: "info-box",
+                    h3 { "Object Info" }
+                    p { "Name: {point.read().metadata.name}" }
+                    p { "ID: {point.read().metadata.id}" }
+                }
+                
+                div { class: "control-box",
+                    h3 { "Rotation" }
+                    label { "Angle: {angle}°" }
+                    input { 
+                        r#type: "range", min: "0", max: "360", value: "{angle}",
+                        oninput: move |evt| {
+                            let new_angle: f64 = evt.value().parse().unwrap_or(0.0);
+                            angle.set(new_angle);
+                            
+                            let origin = geometry::Point2D::new(200.0, 200.0);
+                            let mut new_p = VPoint::new(1, "Point A", 150.0, 150.0);
+                            new_p.coords = new_p.coords.rotate(&origin, new_angle);
+                            point.set(new_p);
+                        }
+                    }
+                }
+            }
+
+            div { id: "viewport",
+                div {
+                    class: "virtual-point",
+                    style: "left: {point.read().x()}px; top: {point.read().y()}px;",
+                    "•"
+                }
+            }
+        }
+    }
 }
